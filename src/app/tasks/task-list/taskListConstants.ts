ea3362aa9b865d76b7936ffe57c879363321c397
amitpanda007 <amitpanda007@gmail.com>
2021-09-10 13:10:45 +0530

    added task list constants file

---
+++ src/app/tasks/task-list/taskListConstants.ts
@@ -0,0 +1,10 @@
+export const Constants = {
+    DUE_OPTIONS: [
+        {text: 'Has no due date', selected: false},
+        {text: 'Due in the next day', selected: false},
+        {text: 'Due in the next week', selected: false},
+        {text: 'Due in the next month', selected: false},
+        {text: 'Overdue', selected: false},
+        {text: 'Due date marked complete', selected: false},
+        {text: 'Not marked as complete', selected: false}]
+}
